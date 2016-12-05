843529600819a2c01f7495a95c1570c822ceda6c
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-12-05 17:58:35 -0500

    use babel to compile es6

---
+++ src/personality-trait-names.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+
+const _ = require('underscore'),
+    extend = _.extend,
+    pick   = _.pick,
+    pairs  = _.pairs;
+
+const I18nDataV2 = require('./i18n/v2');
+const I18nDataV3 = require('./i18n/v3');
+const defaultVersion = 'v2';
+
+
+module.exports = class {
+
+    constructor(options) {
+    //parameters - locale and version
+        this._options = extend(this.defaultOptions(), pick(options, ['locale','version']));
+        this._version = typeof this._options.version !== 'undefined' ? this._options.version : defaultVersion;
+
+        if (this._version === 'v3'){
+            this._i18n = new I18nDataV3(this._options.locale);
+        } else{
+            this._i18n = new I18nDataV2(this._options.locale);
+        }
+        this._data = this._i18n.data();
+    }
+
+    defaultOptions() {
+        return {
+            locale : 'en'
+        };
+    }
+
+    name(traitId) {
+        return this._data[traitId];
+    }
+
+    names() {
+        return pairs(this._data).map(p => p[1]);
+    }
+
+};
